9a96f444c5371e641d29addddde8214955c3e7d4
GeorgePauly95 <georgepauly1995@gmail.com>
2026-02-17 16:14:22 +0530

    WIP

--- static/client.js
+++ static/client.js
@@ -1,14 +1,19 @@
 // use let, var, const for data that is not changing
 // any variable that changes value should not be in the global scope 
+// DONE: Break it into two parts. sendCurrentPosition function is doing too many things, and the name is wrong:
+// DONE: 1. get coordinate data
+// DONE: 2. calculate distance (including updating previous location)
+// DONE: 3. DOM update 
+// DONE: use getElementById since there's only one instance of speed, distance, and time.
+// DO LATER: (not clear how this reduces work) read values from the DOM once, assign variables these values and use the variables. instead of reading DOM multiple times.
 
 var Geolocation = window.navigator.geolocation
 
-let previous_latitude = null
-let previous_longitude = null
+const button = document.getElementById("record")
 
-button = document.getElementById("record")
 
 button.addEventListener("click", recordActivity)
+
 
 function haversineDistance(lat1, lon1, lat2, lon2) {
   const R = 6371; // Earth's radius in km
@@ -25,70 +30,100 @@ function haversineDistance(lat1, lon1, lat2, lon2) {
 
 
 function createStopButton() {
-  stop = document.getElementById("record")
+  const stop = document.getElementById("record")
   stop.style.backgroundColor = "red"
   stop.innerText = "STOP"
 }
 
+
 function createStartButton() {
-  start = document.getElementById("record")
+  const start = document.getElementById("record")
   start.style.backgroundColor = "green"
   start.innerText = "START"
 }
 
-// sendCurrentPosition function is doing too many things, and the name is wrong
-// Break it into two parts:
-// 1. get coordinate data
-// 2. calculate distance (including updating previous location)
-// 3. DOM update 
 
-function sendCurrentPosition(GeolocationPosition) {
-  const coords = GeolocationPosition.coords;
-  const coordsJSON = (coords.toJSON())
+function getCurrentPosition(GeolocationPosition) {
+  let coords = GeolocationPosition.coords;
+  let coordsJSON = (coords.toJSON())
   console.log(JSON.stringify(coordsJSON))
-  current_latitude = coordsJSON["latitude"]
-  current_longitude = coordsJSON["longitude"]
+  let current_latitude = coordsJSON["latitude"]
+  let current_longitude = coordsJSON["longitude"]
+  return { "current_latitude": current_latitude, "current_longitude": current_longitude }
+}
+
+
+let previous_latitude = null
+let previous_longitude = null
+
+function calculateDistanceMoved(current_position) {
+  let current_latitude = current_position["current_latitude"]
+  let current_longitude = current_position["current_longitude"]
+
   if (previous_latitude == null && previous_longitude == null) {
     previous_latitude = current_latitude
     previous_longitude = current_longitude
   }
 
-  // DONE: use getElementById since there's only one instance of speed, distance, and time.
-  // read values from the DOM once, assign variables these values and use the variables. instead of reading DOM multiple times.
-  distance = document.getElementById("distance")
-  distance_travelled = haversineDistance(previous_latitude, previous_longitude, current_latitude, current_longitude) * 1000
+  distance_moved = haversineDistance(previous_latitude, previous_longitude, current_latitude, current_longitude) * 1000
+
   previous_latitude = current_latitude
   previous_longitude = current_longitude
-  distance.innerText = Number(distance.innerText) + distance_travelled
-  speed = document.getElementById("speed")
-  time = document.getElementById("time")
+
+  return distance_moved
+}
+
+
+function updateMetricsScreen(distance_moved) {
+  const distance = document.getElementById("distance")
+  const speed = document.getElementById("speed")
+  const time = document.getElementById("time")
+  distance.innerText = Number(distance.innerText) + distance_moved
   if (time.innerText != 0) {
     speed.innerText = distance.innerText / time.innerText
   }
 }
 
+
+function trackPosition(GeolocationPosition) {
+  let current_position = getCurrentPosition(GeolocationPosition)
+  let distance_moved = calculateDistanceMoved(current_position)
+  updateMetricsScreen(distance_moved)
+}
+
+
 function timeWorkout() {
-  time = document.getElementById("time")
+  const time = document.getElementById("time")
   time.innerText = Number(time.innerText) + 1
 }
+
+
+let time_id
+let id
 
 function startActivity() {
   createStopButton()
   time_id = setInterval(timeWorkout, 1000)
-  id = Geolocation.watchPosition(sendCurrentPosition)
+  console.log("time_id in startActivity", time_id)
+  id = Geolocation.watchPosition(trackPosition)
 }
+
 
 function stopActivity() {
   createStartButton()
+  console.log("time_id in stopActivity", time_id)
   clearInterval(time_id)
   Geolocation.clearWatch(id)
 }
 
+
 function recordActivity() {
-  button = document.getElementById("record")
+  const button = document.getElementById("record")
+
   if (button.innerText == "START") {
     startActivity()
   }
+
   else {
     stopActivity()
   }
