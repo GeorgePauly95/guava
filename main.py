d484ba2b362d3d9e64ee4e7355283d5251469ac1
GeorgePauly95 <georgepauly1995@gmail.com>
2026-02-23 11:21:26 +0530

    WIP

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# @app.websocket("/api/ws")
+# async def track_position(websocket: WebSocket):
