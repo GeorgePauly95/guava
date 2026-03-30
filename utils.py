50eade2344678af90dca91b7793a420cb565d7b8
GeorgePauly95 <georgepauly1995@gmail.com>
2026-03-30 16:47:11 +0530

    demo changes 1

--- utils.py
+++ utils.py
@@ -1,12 +1,17 @@
-def status_code_map(status):
-    if status == "WORKOUT_NOT_FOUND":
+def status_code_map(outcome_status):
+    if outcome_status == "WORKOUT_NOT_FOUND":
         return 404
-    elif status in {
+    elif outcome_status in {
         "WORKOUT_ALREADY_COMPLETED",
         "WORKOUT_NOT_PAUSED",
         "WORKOUT_ALREADY_PAUSED",
     }:
         return 409
-    elif status == "BAD_REQUEST":
-        return 400
     return 200
+
+
+def calculate_time(locations):
+    first_location = locations[0]
+    latest_location = locations[-1]
+    time = latest_location["time"] - first_location["time"]
+    return str(time)
