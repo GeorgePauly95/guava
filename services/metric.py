0d09cfaae5150c905a0027d400d546302b24fd36
GeorgePauly95 <georgepauly1995@gmail.com>
2026-05-22 12:14:45 +0530

    logic to send metrics to users with active workouts and connections

--- services/metric.py
+++ services/metric.py
@@ -57,15 +57,44 @@ async def get_metrics(workout_id: int):
 async def update_metrics(connection_manager):
     while True:
         await asyncio.sleep(5)
-        print("active connections:", connection_manager.active_connections)
         try:
-            for user_id, websocket in connection_manager.active_connections.items():
-                workout_id = Workouts.get_active_workouts_for_user(user_id)
-                if workout_id is None:
-                    continue
-                metrics = await get_metrics(workout_id)
-                if metrics is None:
-                    continue
-                await websocket.send_text(json.dumps(metrics))
+            user_ids = list(connection_manager.active_connections.keys())
+            active_workouts = Workouts.get_active_workouts_for_users(user_ids)
+            active_connections = list(connection_manager.active_connections.items())
+            print(
+                "active_workouts:",
+                active_workouts,
+                "\nactive_connections:",
+                active_connections,
+            )
+            for user_id, websocket in active_connections:
+                for active_workout in active_workouts:
+                    if active_workout["user_id"] == user_id:
+                        workout_id = active_workout["id"]
+                        if workout_id is None:
+                            continue
+                        metrics = await get_metrics(workout_id)
+                        if metrics is None:
+                            continue
+                        await websocket.send_text(json.dumps(metrics))
+                    else:
+                        return
         except Exception as e:
             print("Error: ", e)
+
+
+# async def update_metrics(connection_manager):
+#     while True:
+#         await asyncio.sleep(5)
+#         print("active connections:", connection_manager.active_connections)
+#         try:
+#             for user_id, websocket in connection_manager.active_connections.items():
+#                 workout_id = Workouts.get_active_workouts_for_user(user_id)
+#                 if workout_id is None:
+#                     continue
+#                 metrics = await get_metrics(workout_id)
+#                 if metrics is None:
+#                     continue
+#                 await websocket.send_text(json.dumps(metrics))
+#         except Exception as e:
+#             print("Error: ", e)
